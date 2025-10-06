265de2198f3d268d5bd489605e236c5bf56868d4
Felipe Oyarzun <oyarzunfelipe6@gmail.com>
2025-10-06 17:03:09 -0300

    feat(di): Integrate Koin and data module
    Integrates the Koin dependency injection framework into the application and sets up the initial DI graph.
    
    - Adds the Koin framework to the `LazyPizzaApp` class using `startKoin`.
    - Configures Koin with `androidContext` and `androidLogger`.
    - Introduces and includes the `dataModule` from the `:core:data` module into the application's Koin configuration, making data-layer dependencies available for injection.

--- app/src/main/java/com/fomaxtro/lazypizza/LazyPizzaApp.kt
+++ app/src/main/java/com/fomaxtro/lazypizza/LazyPizzaApp.kt
@@ -1,6 +1,7 @@
 package com.fomaxtro.lazypizza
 
 import android.app.Application
+import com.fomaxtro.core.data.di.dataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -18,7 +19,9 @@ class LazyPizzaApp : Application() {
             androidContext(this@LazyPizzaApp)
             androidLogger()
 
-            modules()
+            modules(
+                dataModule
+            )
         }
     }
 }
